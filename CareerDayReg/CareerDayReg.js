06892430d37881f2c5b7127d9f37e09f06d25609
Spencer Lanman <spencer@scanpower.com>
2014-06-05 17:51:11 +0000

    Added time logging on registration, CSV file generation, and success page

--- CareerDayReg/CareerDayReg.js
+++ CareerDayReg/CareerDayReg.js
@@ -41,6 +41,10 @@ if (Meteor.isClient) {
             return _.range(1, students[0].classnames.length+1);
         else return [];
     });
+    
+    Handlebars.registerHelper('getCurrentUsername', function(){
+        return Meteor.user().emails[0].address;
+    });
 
     function liveCount(classname) {
         var count = Students.find({classnames: classname}).fetch().length;
@@ -55,6 +59,21 @@ if (Meteor.isClient) {
     Template.admin.events({
         'click input.signout': function(){ 
             Meteor.logout();
+        },
+
+        'click input.login': function(){
+            var username = $('input.username').val();
+            var password = $('input.password').val();
+
+            Meteor.loginWithPassword(username, password);
+        },
+        
+        'click input.getcsv': function(){
+            Meteor.call('generateCsvFile', function(error, filename){
+                if(error) throw error;
+                console.log(data);
+                Router.to(filename);
+            });
         }
     });
     
@@ -64,7 +83,7 @@ if (Meteor.isClient) {
       		var lname = $('input.lname').val();
             var schoolname = $('select.schoolselect :selected').val();
             var checked = $('input.classcheck:checked');
-        
+
             if(fname!="" && lname!="") {
             if(schoolname!="none") {
             if(checked.length==3) {
@@ -82,20 +101,23 @@ if (Meteor.isClient) {
                 i++;
             });
 
-            if(!overflowed) {
+            if(!overflowed) {    
                 for(var i=0; i<ids.length; i++) {
                     classnames[i] = Classes.findOne({_id: ids[i]}).classname;
                 }
+                    
+                Meteor.call('getServerTime', function(error, time) {
+                    Students.insert({fname:fname, lname:lname, schoolname:schoolname, classnames:classnames, time:time});
+                });
 
-                Students.insert({fname:fname, lname:lname, schoolname:schoolname, classnames:classnames});
-
-      		    Session.set("registermsg", "Successfully Registered");
                 $('input.register').prop("disabled", true);
                 setTimeout(function(){
-                    $('input.register').prop("disabled", false);
+                $('input.register').prop("disabled", false);
                 }, 5000);
+                
+                Session.set("registermsg", "");
 
-                Router.go('home');
+                Router.go('success');
             } else
                 Session.set("registermsg", "One or more of your requested classes is already full");
             } else
@@ -109,6 +131,40 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+    
+    csv = Meteor.require('fast-csv');
+    fs = Meteor.require('fs');
+    
+    Router.map(function(){
+        this.route('downloadFile', {where: 'server', path: '/files/:filename/', action: function(){
+                var filename = this.params.filename;
+                this.response.writeHead(200, {'Content-Type':'text/csv'});
+                this.response.end(fs.readFileSync('/tmp/'+filename));
+        }})});
+
+    Meteor.methods({
+        getServerTime: function() {
+            var time = (new Date).toString();
+            return time;
+        },
+
+        generateCsvFile: function() {
+            var csvStream = csv.createWriteStream({headers: true});
+            writableStream = fs.createWriteStream("/tmp/" + "roster"  + ".csv");
+            writableStream.on('finish', function(){
+                console.log("Done generating CSV file");
+            });
+
+            csvStream.pipe(writableStream);       
+
+            var roster = Students.find().fetch();
+
+            for(var i=0; i<roster.length; i++) csvStream.write({FirstName: roster[i].fname, LastName: roster[i].lname, School: roster[i].schoolname, Class1: roster[i].classnames[0], Class2: roster[i].classnames[1], Class3: roster[i].classnames[2], Time: roster[i].time});
+            
+            csvStream.write(null);
+            return "roster";
+        }
+    });
 
   	Meteor.startup(function () {
         if (Classes.find().count() === 0)
--- CareerDayReg/Routes.js
+++ CareerDayReg/Routes.js
@@ -1,6 +1,7 @@
 Router.map(function() {
     this.route('home', {path: '/', layoutTemplate: 'main'});
     this.route('register', {path: '/register/', layoutTemplate: 'main'});
+    this.route('success', {path: '/success/', layoutTemplate: 'main'});
 
     this.route('roster', {path: '/admin/controlpanel/roster/', layoutTemplate: 'admin'});
     this.route('schedule', {path: '/admin/controlpanel/schedule/', layoutTemplate: 'admin'});
